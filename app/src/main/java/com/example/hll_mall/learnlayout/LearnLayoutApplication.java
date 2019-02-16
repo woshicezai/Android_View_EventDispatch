9f482dd40335feb3236c3156e59b7682324f745f
zhouce <zhouce@hualala.com>
2019-02-16 15:07:18 +0800

    创建项目

---
+++ app/src/main/java/com/example/hll_mall/learnlayout/LearnLayoutApplication.java
@@ -0,0 +1,12 @@
+package com.example.hll_mall.learnlayout;
+
+import android.app.Application;
+
+public class LearnLayoutApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/hll_mall/learnlayout/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.hll_mall.learnlayout;
+
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.alibaba.android.arouter.facade.Postcard;
+import com.alibaba.android.arouter.facade.callback.NavCallback;
+import com.alibaba.android.arouter.launcher.ARouter;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    public void initView(){
+
+    }
+
+
+
+}
